a957fea617b23a5ef6c374268307ce1397f3a939
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-06-13 23:06:54 +0200

    add functionalities for buttons

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,25 @@ registerSW();
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const number = document.querySelector(".number--js");
+const add = document.querySelector(".add--js");
+const remove = document.querySelector(".remove--js");
+
+let counter = 0;
+
+number.innerHTML = counter;
+
+add.addEventListener('click', () => {
+    counter++
+    number.innerHTML = counter ;
+});
+
+remove.addEventListener ('click', () => {
+    if (counter>0) {
+    counter--
+    number.innerHTML = counter 
+    } else {
+        counter = 0
+    };
+});
